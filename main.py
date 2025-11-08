f01e0266692c27e6fc1a30c6f8bbc28eb4715633
xMDxKevin <kevinmontes543rt@gmail.com>
2025-11-08 00:55:48 -0500

    fix: Se corrigió CORS para permitir peticiones desde https://serenity-zz.vercel.app

--- main.py
+++ main.py
@@ -42,16 +42,14 @@ db_pool: asyncpg.Pool | None = None
 app = FastAPI(title="Serenity Zero API", description="API de Serenity Zero", version="1.0.0")
 
 origins = [
-    "URL_DE_TU_FRONTEND_AQUI", 
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
+    "https://serenity-zz.vercel.app", 
+    "http://localhost:3000", 
     "http://localhost:5173", 
-    "https://serenity-zz.vercel.app/",
 ]
 
 app.add_middleware(
 CORSMiddleware,
-allow_origins=origins,
+allow_origins=origins, 
 allow_credentials=True,
 allow_methods=["*"],
 allow_headers=["*"],
